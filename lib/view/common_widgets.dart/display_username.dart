f990604af5522bf257c47e0fc92d072b7ee69392
Abhijith VM <abhijithmadhusoodanan@gmail.com>
2024-04-11 17:56:32 +0530

    task 3

---
+++ lib/view/common_widgets.dart/display_username.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DisplayName extends StatelessWidget {
+  final String firstName;
+  final String secondName;
+  final String prefix;
+  final String suffix;
+
+  const DisplayName({
+    super.key,
+    required this.firstName,
+    required this.secondName,
+    this.prefix = '',
+    this.suffix = '',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$prefix. $firstName $secondName $suffix',
+      style: const TextStyle(fontSize: 16), // Adjust the font size as needed
+    );
+  }
+}
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trippldee/view/common_widgets.dart/country_state_widget.dart';
+import 'package:trippldee/view/common_widgets.dart/display_username.dart';
 
 import 'common_widgets.dart/dropdown_widget.dart';
 
@@ -15,14 +16,29 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            CustomDropdownWidget(),
-            CountryStateDropdown(),
+            customGapFunction("Task 1"),
+            const CustomDropdownWidget(),
+            const SizedBox(height: 12),
+            customGapFunction("Task 2"),
+            const CountryStateDropdown(),
+            customGapFunction("Task 3"),
+            const DisplayName(
+              firstName: "Abhijith",
+              secondName: "VM",
+              prefix: "Mr",
+              suffix: "Thrissur",
+            )
           ]),
         ),
       ),
     );
   }
 }
+
+Widget customGapFunction(String task) => Padding(
+      padding: const EdgeInsets.only(top: 12),
+      child: Text(task),
+    );
